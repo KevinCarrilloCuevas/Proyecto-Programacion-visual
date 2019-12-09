d18b4944076589e659112cd6f52388cafd35b72d
KevinCarrilloCuevas <43249589+KevinCarrilloCuevas@users.noreply.github.com>
2019-12-09 01:40:02 -0500

    Ultimas correcciones
    Se agregaron condiciones para el ComboBox, se Agregaron Condiciones para los radio buttoms y se agregaron iconos a los messagebox

--- Registro de usuarios/Registro de usuarios/Form1.vb
+++ Registro de usuarios/Registro de usuarios/Form1.vb
@@ -1,12 +1,14 @@
 ﻿Public Class Form1
 
 
+
+
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
 
 
 
         If Trim(TextBox1.Text) = "" Or Trim(TextBox5.Text) = "" Then
-            MessageBox.Show("Algunos Campos estan vacios")
+            MessageBox.Show("Algunos campos del usuario estan vacios, Verifiquelos", "Usuario", MessageBoxButtons.OK, MessageBoxIcon.Error)
 
         Else
             MessageBox.Show("Usuario Registrado Con Exito")
@@ -16,34 +18,52 @@
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
         If Trim(TextBox2.Text) = "" Or Trim(TextBox3.Text) = "" Or Trim(TextBox4.Text) = "" Then
-            MessageBox.Show("Algunos Campos estan vacios Verifiquelos")
+            MessageBox.Show("Algunos campos estan vacios, Verifiquelos", "Datos", MessageBoxButtons.OK, MessageBoxIcon.Error)
 
         Else
             MessageBox.Show("Datos Guardado Con Exito ")
 
         End If
+
+
+
         If (ComboBox1.SelectedIndex = -1) Then
 
-            MessageBox.Show("NO Ha elegido ninguna opcion en STATUS")
+            MessageBox.Show("No Ha elegido ninguna opcion en STATUS", "Usuario", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
         End If
 
         If (ComboBox2.SelectedIndex = -1) Then
 
-            MessageBox.Show("NO Ha elegido UN TIPO DE USUARIO")
+            MessageBox.Show("No Ha elegido UN TIPO DE USUARIO", "Usuario", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
         End If
 
 
-        If RadioButton1.Checked = 0 Or RadioButton2.Checked = 0 Then
 
-            MessageBox.Show("NO Has elegido ningun GENERO")
+
+        If CheckedListBox1.CheckedItems.Count < 1 Then
+            MessageBox.Show("tienes que elegir al menos un puesto de trabajo", "Datos", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+
+        ElseIf CheckedListBox1.CheckedItems.Count > 1 Then
+
+            MessageBox.Show("Solo puedes elegir un puesto de trabajo", "Datos", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        End If
+
+
+
+        If RadioButton1.Checked Then
+
+        ElseIf RadioButton2.Checked Then
 
         Else
-            MessageBox.Show("Genero guardado")
+            MessageBox.Show("No eligio un genero", "genero", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
         End If
-
-
 
 
     End Sub
+
 End Class
